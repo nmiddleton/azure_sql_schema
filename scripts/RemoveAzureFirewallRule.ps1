afe0b3de02a9ec2c4d642c57b946bb4556139f7a
Neil Middleton <Neil.Middleton@kennedyslaw.com>
2019-03-01 16:01:34 +0000

    set default sub in scripts

--- scripts/RemoveAzureFirewallRule.ps1
+++ scripts/RemoveAzureFirewallRule.ps1
@@ -9,5 +9,6 @@ $ErrorActionPreference = 'Stop'
 
 If ((Get-AzureSqlDatabaseServerFirewallRule -ServerName $ServerName -RuleName $AzureFirewallName -ErrorAction SilentlyContinue))
 {
+  Set-AzureSubscription -SubscriptionId '51af872a-b560-413b-bb0f-8a11e0f8ce64'
   Remove-AzureSqlDatabaseServerFirewallRule -RuleName $AzureFirewallName -ServerName $ServerName
 }
