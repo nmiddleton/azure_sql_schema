942298c6862d565e718d659992b60b970c70eec8
Neil Middleton <Neil.Middleton@kennedyslaw.com>
2019-03-04 10:53:51 +0000

    hardcode id

--- scripts/SetAzureFirewallRule.ps1
+++ scripts/SetAzureFirewallRule.ps1
@@ -4,7 +4,8 @@ param
     [String] [Parameter(Mandatory = $true)] $ServerName,
     [String] $AzureFirewallName = "AzureWebAppFirewall"
 )
-Add-AzureAccount
+Add-AzureRMAccount -ServicePrincipal -Tenant 'f07f8b10-167e-4185-a4ba-27d528351285' -Credential System.Management.Automation.PSCredential -Environment AzureCloud @processScope
+Select-AzureRMSubscription -SubscriptionId 51af872a-b560-413b-bb0f-8a11e0f8ce64 -TenantId 'f07f8b10-167e-4185-a4ba-27d528351285'
 #$ErrorActionPreference = 'Stop'
 
 function New-AzureSQLServerFirewallRule {
